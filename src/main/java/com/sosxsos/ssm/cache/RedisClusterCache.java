9bd7b2e0ff88b46d8ebcee1185e21ad79302477a
peng <yinpengroc@gmail.com>
2017-10-27 12:57:27 +1300

    changeDB

--- src/main/java/com/sosxsos/ssm/cache/RedisClusterCache.java
+++ src/main/java/com/sosxsos/ssm/cache/RedisClusterCache.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.JedisPool;
-import java.util.HashSet;
+//import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
